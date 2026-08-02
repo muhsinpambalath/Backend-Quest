110acb8840ca0800ecdd7b09e00347e6de85e19e
muhsinpambalath <muhsinpambalath@gmail.com>
2026-08-02 17:13:16 +0530

    feat(level7): implement HTTP server with routing

--- level7/server.js
+++ level7/server.js
@@ -1,10 +1,24 @@
 import http from "http";
 import fs from "fs";
 
-const html = fs.readFileSync("./index.html","utf-8");
+const home = fs.readFileSync("./index.html","utf-8");
+const about = fs.readFileSync("./about.html","utf-8");
+const contact = fs.readFileSync("./contact.html","utf-8");
 
 const server = http.createServer((req,res)=>{
-    res.end(html);
+    res.setHeader("Content-Type","text/html");
+    if(req.url === "/"){
+        res.end(home);
+    }
+    else if(req.url==="/about"){
+        res.end(about);
+    }
+    else if(req.url === "/contact"){
+        res.end(contact);
+    }
+    else{
+        res.end(`<h2 style="text-align: center;"> <b> 404 ERROR </b> </h2>`);
+    }
 });
 
 server.listen(3000);
